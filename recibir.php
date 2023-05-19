ba5307bf0e87d2dbf355807004135497110be842
cecimqz <130602303+cecimqz@users.noreply.github.com>
2023-05-19 00:16:37 -0300

    Create recibir.php

---
+++ recibir.php
@@ -0,0 +1,38 @@
+<?php
+  if(isset($_POST["Nombre"])){ //Verificamos que la variable esta seteada
+    $dato = $_POST["Nombre"]; //La guardamos dentro de una variable mas amigable
+    echo $dato; //Ya que esta guardada, la podemos imprimir
+  }
+ if(isset($_POST["Apellido"])){ //Verificamos que la variable esta seteada
+    $dato = $_POST["Apellido"]; //La guardamos dentro de una variable mas amigable
+    echo $dato; //Ya que esta guardada, la podemos imprimir
+  }
+ if(isset($_POST["DNI"])){ //Verificamos que la variable esta seteada
+    $dato = $_POST["DNI"]; //La guardamos dentro de una variable mas amigable
+    echo $dato; //Ya que esta guardada, la podemos imprimir
+  }
+ if(isset($_POST["Nacimiento"])){ //Verificamos que la variable esta seteada
+    $dato = $_POST["Nacimiento"]; //La guardamos dentro de una variable mas amigable
+    echo $dato; //Ya que esta guardada, la podemos imprimir
+  }
+ if(isset($_POST["01"])){ //Verificamos que la variable esta seteada
+    $dato = $_POST["01"]; //La guardamos dentro de una variable mas amigable
+    echo $dato; //Ya que esta guardada, la podemos imprimir
+  }
+ if(isset($_POST["02"])){ //Verificamos que la variable esta seteada
+    $dato = $_POST["02"]; //La guardamos dentro de una variable mas amigable
+    echo $dato; //Ya que esta guardada, la podemos imprimir
+  }
+ if(isset($_POST["03"])){ //Verificamos que la variable esta seteada
+    $dato = $_POST["03"]; //La guardamos dentro de una variable mas amigable
+    echo $dato; //Ya que esta guardada, la podemos imprimir
+  }
+ if(isset($_POST["04"])){ //Verificamos que la variable esta seteada
+    $dato = $_POST["04"]; //La guardamos dentro de una variable mas amigable
+    echo $dato; //Ya que esta guardada, la podemos imprimir
+  }
+ if(isset($_POST["Comentarios"])){ //Verificamos que la variable esta seteada
+    $dato = $_POST["Comentarios"]; //La guardamos dentro de una variable mas amigable
+    echo $dato; //Ya que esta guardada, la podemos imprimir
+  }
+?>
