8fa453f839bc22bdce2d4b54f1ab61b098b83909
maguuleal <smagalileal@hotmail.com>
2023-07-09 20:02:34 -0300

    asdasd

--- borrador cosas profe.py
+++ borrador cosas profe.py
@@ -2,45 +2,14 @@ import sqlite3
 from flask import Flask,  jsonify, request
 from flask_cors import CORS
 
-
 # Configurar la conexión a la base de datos SQLite
-DATABASE = 'inventario.db'
+DATABASE = 'productos.db'
 
-def get_db_connection():
+def conectar():
     conn = sqlite3.connect(DATABASE)
     conn.row_factory = sqlite3.Row
     return conn
 
-# Crear la tabla 'productos' si no existe
-def create_table():
-    conn = get_db_connection()
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS productos (
-            codigo INTEGER PRIMARY KEY,
-            descripcion TEXT NOT NULL,
-            cantidad INTEGER NOT NULL,
-            precio REAL NOT NULL
-        )
-    ''')
-    conn.commit()
-    cursor.close()
-    conn.close()
-
-# Verificar si la base de datos existe, si no, crearla y crear la tabla
-def create_database():
-    conn = sqlite3.connect(DATABASE)
-    conn.close()
-    create_table()
-
-# Crear la base de datos y la tabla si no existen
-create_database()
-
-#-------------------------------------------------------------------
-#ETAPA 3
-# Ejemplo de uso de las clases y objetos definidos antes:
-# -------------------------------------------------------------------
-# Crear una instancia de la clase Inventario
 x = Inventario()
 
 # Crear una instancia de la clase Carrito
